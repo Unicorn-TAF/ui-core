d17b90d52d207ec8f929e35b564a0f63976cbf37
Vitaliy Dobriyan <dobriy88@yandex.ru>
2023-09-06 23:54:05 +0300

    added unit tests

---
+++ src/Unicorn.UnitTests/FakeControls/UiControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using Unicorn.UI.Core.Controls;
+using Unicorn.UI.Core.Driver;
+
+namespace Unicorn.UnitTests.FakeControls
+{
+    internal class UiControl : UiSearchContext, IControl
+    {
+        public bool Cached { get; set; }
+
+        public ByLocator Locator { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Visible => throw new NotImplementedException();
+
+        public bool Enabled => throw new NotImplementedException();
+
+        public string Text => throw new NotImplementedException();
+
+        public Point Location => throw new NotImplementedException();
+
+        public Rectangle BoundingRectangle => throw new NotImplementedException();
+
+        public void Click()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAttribute(string attribute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RightClick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Unicorn.UnitTests/Tests/ContainerInitTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Unicorn.UI.Core.Driver;
+using Unicorn.UI.Core.PageObject;
+using Unicorn.UnitTests.FakeControls;
+
+namespace Unicorn.UnitTests.Tests
+{
+    [TestFixture]
+    internal class ContainerInitTests
+    {
+        private UiContainer container;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            container = new UiContainer();
+            ContainerFactory.InitContainer(container);
+        }
+
+        [Test]
+        public void PageObjectFieldInit()
+        {
+            UiControl control = container.GetUiControl();
+            Assert.That(control.Locator.How, Is.EqualTo(Using.Class));
+            Assert.That(control.Locator.Locator, Is.EqualTo(UiContainer.ControlFieldClass));
+        }
+
+        [Test]
+        public void PageObjectPropertyInit()
+        {
+            UiControl control = container.ControlProperty;
+            Assert.That(control.Locator.How, Is.EqualTo(Using.WebCss));
+            Assert.That(control.Locator.Locator, Is.EqualTo(UiContainer.ControlPropertyCss));
+        }
+
+        [Test]
+        public void PageObjectCustomControlPropertyInit()
+        {
+            UiControl control = container.CustomControlProperty;
+            Assert.That(control.Locator.How, Is.EqualTo(Using.Id));
+            Assert.That(control.Locator.Locator, Is.EqualTo(CustomControl.CustomControlId));
+        }
+
+        [Test]
+        public void PageObjectTemplatedControlInit()
+        {
+            UiControl control = container.GetTemplatedCustomControl();
+            Assert.That(control.Locator.How, Is.EqualTo(Using.WebXpath));
+
+            string expectedLocator = string.Format(TemplatedCustomControl.TemplatedCustomControlXpath, 
+                UiContainer.ControlFieldClass);
+
+            Assert.That(control.Locator.Locator, Is.EqualTo(expectedLocator));
+        }
+    }
+}
